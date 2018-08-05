7e8bb1ebd48e700d970d4ef63f17d1f993b30a5f
PauIGB <56a06@mail.ru>
2018-08-05 19:12:52 +0300

    waxom

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".slider").owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1000,
+        navText : ["",""]
+    });
+});
